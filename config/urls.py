71fc152ffed3fbd13e75da0ef4d33a154a94c5fc
EguseV <ekgusev3000@gmail.com>
2025-03-13 22:11:34 +0300

    Aded more pages

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from weather.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home, name='home'),
+    path('weather-forecast/', weather_forecast, name='weather_forecast'),
+    path('weather-map/', weather_map, name='weather_map'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('privacy-policy/', privacy_policy, name='privacy_policy'),
+    path('terms-of-service/', terms_of_service, name='terms_of_service'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django import forms
+import random
 
-# Create your views here.
+
+def home(req):
+    return render(req, 'main/index.html')
+
+
+def weather_forecast(request):
+    return render(request, 'weather/weather_forecast.html')  # Шаблон для прогноза погоды
+
+
+def weather_map(request):
+    return render(request, 'weather/weather_map.html')  # Шаблон для карты погоды
+
+
+def about(request):
+    return render(request, 'weather/about.html')  # Шаблон для страницы "О нас"
+
+
+def contact(request):
+    return render(request, 'weather/contact.html')  # Шаблон для страницы "Контакты"
+
+
+def privacy_policy(request):
+    return render(request, 'weather/privacy_policy.html')  # Шаблон для страницы "Политика конфиденциальности"
+
+
+def terms_of_service(request):
+    return render(request, 'weather/terms_of_service.html')  # Шаблон для страницы "Условия использования"4
